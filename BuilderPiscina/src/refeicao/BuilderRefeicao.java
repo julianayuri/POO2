c9d4e1fada51624fa69d0b3146ebface850ab6a5
Juliana Yuri <julianakanezaki@gmail.com>
2017-03-21 09:00:11 -0300

    Add files via upload

---
+++ BuilderPiscina/src/refeicao/BuilderRefeicao.java
@@ -0,0 +1,19 @@
+
+package refeicao;
+
+/**
+ *
+ * @author felip
+ */
+public class BuilderRefeicao {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    /**
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }*/
+    
+}
---
+++ BuilderPiscina/src/refeicao/RefeicaoBuilder.java
@@ -0,0 +1,24 @@
+
+
+package refeicao;
+
+/**
+ *
+ * @author felipe
+ */
+public abstract class RefeicaoBuilder {    
+    Refeicao refeicao;        
+    
+    public void adicionarSuco(String sabor){                                
+        refeicao.setBebida(new Suco(sabor));        
+    }   
+       
+    public void adicionarRefrigerante(String sabor){
+        refeicao.setBebida(new Refrigerante(sabor));        
+    }   
+    
+    public void preparaBatata(){
+        System.out.println("fritando batata");
+        refeicao.setBatata(new Batata());
+    }
+}
